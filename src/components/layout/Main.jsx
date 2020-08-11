6e746c9076e35937f9eff318dd52105db01b0c25
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2020-08-11 14:07:49 +0300

    Added Main component

---
+++ src/components/layout/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+  export function Main(props){
+      return(
+          <div className="Main">
+          <span>Main!</span>
+          </div>
+      )
+  }
