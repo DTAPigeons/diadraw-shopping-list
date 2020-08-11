eef1e4dc9115e0fe62b552cc1961b1268508d13a
DESKTOP-PEJCES4\DeathToAllPigeons <pavkat97@gmail.com>
2020-08-11 10:05:34 +0300

    Initail commit

---
+++ src/core/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlUG6DEzznIyuW5_iwh-oE6zm4T4qqIg4",
+    authDomain: "shoping-list-f5efb.firebaseapp.com",
+    databaseURL: "https://shoping-list-f5efb.firebaseio.com",
+    projectId: "shoping-list-f5efb",
+    storageBucket: "shoping-list-f5efb.appspot.com",
+    messagingSenderId: "531706082763",
+    appId: "1:531706082763:web:da57a2b3f43031ec0ab24e"
+  };
+
+  export default firebaseConfig;
